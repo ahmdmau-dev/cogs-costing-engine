2235f984e248ad3653074af058aca104bb15016c
Ahmad Maulana <ahmad.maulana@kapalapi.co.id>
2026-06-20 09:03:03 +0700

    chore: scaffold NestJS + TypeORM project

---
+++ src/data-source.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL ?? 'postgres://ictkapalapiglobal@localhost:5432/cogs',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: false,
+});
